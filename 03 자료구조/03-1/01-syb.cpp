15777970a2fc97cf99fa15e1c628bb2f3bda7acd
youngbin03 <87307678+youngbin03@users.noreply.github.com>
2024-01-25 23:06:35 +0900

    Update 01-syb.cpp

--- 03 자료구조/03-1/01-syb.cpp
+++ 03 자료구조/03-1/01-syb.cpp
@@ -1,1 +1,18 @@
-01번풀이
+#include <stdio.h>
+
+int main()
+{
+    int a,b,total=0;
+    int num[101] = {};
+    scanf("%d",&a);
+    for(int i=0; i<a; i++)
+    {
+        scanf("%1d",&num[i]);
+    }
+    for(int i=0; i<a; i++)
+    {
+        total += num[i]; 
+    }
+    printf("%d",total);
+    return 0;
+}
