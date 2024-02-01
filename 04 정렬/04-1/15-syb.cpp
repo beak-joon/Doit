48776d6c8ad1768087b6fd5e5f039dbbafb46ecc
youngbin03 <87307678+youngbin03@users.noreply.github.com>
2024-02-02 06:01:19 +0900

    Update 15-syb.cpp

--- 04 정렬/04-1/15-syb.cpp
+++ 04 정렬/04-1/15-syb.cpp
@@ -1,1 +1,16 @@
-hello
+#include <stdio.h>
+int main()
+{
+    int num[1001]={0,}, dnum[1001]={0,},a,b=0;
+    scanf("%d",&a);
+    for(int i=0; i<a; i++){
+        scanf("%d",&b);
+        if(b>=0){num[b] += 1;}
+        else{
+        	b *= -1;
+        	dnum[b] += 1;
+        }
+    }
+    for(int i=1000; i>0; i--) if(dnum[i]!=0) printf("-%d\n",i);
+    for(int i=0; i<1001; i++) if(num[i]!=0) printf("%d\n",i);
+}
