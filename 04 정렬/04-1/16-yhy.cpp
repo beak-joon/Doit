66c8ebea19deb9496c007ff4eed8f277dc6d0a87
hoyeol040820 <149706689+hoyeol040820@users.noreply.github.com>
2024-02-02 00:25:02 +0900

    Add files via upload

---
+++ 04 정렬/04-1/16-yhy.cpp
@@ -0,0 +1,31 @@
+#include <iostream> //1377번
+#include <vector>
+#include <algorithm>
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int N;
+    std::cin>>N;
+    int temp;
+    std::vector<std::pair<int,int> > ve(N);
+    for(int i=0; i<N; i++){
+        std::cin>>ve[i].first; //ve의 각 요소는 (data,index)
+        ve[i].second=i;
+    }
+
+    sort(ve.begin(),ve.end()); //ve를 정렬
+
+    int Max=ve[0].second-0;
+    for(int i=1; i<N; i++){
+        if(Max<ve[i].second-i){
+            Max=ve[i].second-i;
+        }
+    }
+
+    std::cout<<Max<<'\n';
+
+    return 0;
+}
