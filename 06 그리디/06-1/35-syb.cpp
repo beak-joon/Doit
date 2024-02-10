024cb996980121ad17905f23dd5a419f2fd7227a
youngbin03 <87307678+youngbin03@users.noreply.github.com>
2024-02-10 16:30:30 +0900

    Create 35-syb.cpp

---
+++ 06 그리디/06-1/35-syb.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+struct comp{
+    bool operator()(pair<int,int>& v1, pair<int,int>& v2){
+        if(v1.second == v2.second) return v1.first > v2.first;
+        return v1.second > v2.second;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, comp> pq;
+    int n; cin >> n;
+    for(int i=0; i<n; i++){
+        int u,v;
+        cin >> u >> v;
+        pq.push({u,v});
+    }
+    int end_time = -1, result = 0;
+    while(!pq.empty()){
+        int start = pq.top().first;
+        if(start >= end_time){ 
+            end_time = pq.top().second; 
+            result ++;
+        }
+        pq.pop();
+    }
+    cout << result;
+    return 0;
+}
